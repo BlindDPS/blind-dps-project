7c4324f85a667b8fe307f8ef3d9a2ef00697eff4
jeongsol-kim <wjdthf3927@gmail.com>
2023-03-02 19:17:30 +0900

    initialize each section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Header from "./components/header/Header";
+import Section1 from "./components/section1/Section1";
+import Section2 from "./components/section2/Section2";
+import Section3 from "./components/section3/Section3";
+import Section4 from "./components/section4/Section4";
+import Section5 from "./components/section5/Section5";
+import Footer from "./components/footer/Footer";
+
 import { MathJaxContext } from 'better-react-mathjax';
 
 const config = {
@@ -20,22 +28,17 @@ const config = {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MathJaxContext version={3} config={config}>
+      <div>
+        <Header />
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />
+        <Footer />
+      </div>
+    </MathJaxContext>
   );
 }
 
