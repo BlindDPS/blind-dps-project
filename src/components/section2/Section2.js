d32df69929f4efca44d77093fa9083699d6ca7b1
jeongsol-kim <wjdthf3927@gmail.com>
2023-03-11 17:13:29 +0900

    add method section

--- src/components/section2/Section2.js
+++ src/components/section2/Section2.js
@@ -1,11 +1,35 @@
 import React, {Component, Fragment} from "react";
 
-export default class Section2 extends Component{
-    render(){
-        return (
-            <Fragment>
-                
-            </Fragment>
-        );
-    }
+const CenterWrapper = (props) => {
+    return (
+        <section className="section">
+            <div className="container is-max-desktop">
+                <div className="columns is-centered has-text-centered">
+                    <div className="column is-four-fifths">
+                        {props.content}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
+
+const Content = () => {
+    return (
+        <div>
+        <h2 className="title is-3">Method</h2>
+        <img id="method"
+             height={"100%"}
+             src={process.env.PUBLIC_URL + "/imgs/method.gif"}
+             alt={"loading.."}/>
+        </div>
+    )
+}
+
+const Section2 = () => {
+    return (
+        <CenterWrapper content={<Content />}/>
+    );
+}
+
+export default Section2
